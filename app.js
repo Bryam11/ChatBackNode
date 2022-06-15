f44a87e73af4c5067a2eedae48cfd01d7180953a
Bryam11 <xavierchuchuca18@gmail.com>
2022-06-15 17:17:54 -0500

    agree socket io

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const dbConnect = require("./config/mongo");
 const authRoutes = require("./routes/auth");
 const messageRoutes = require("./routes/messages");
+const socket = require("socket.io");
+
 const app = express();
 app.use(cors())
 app.use(express.json())
@@ -17,4 +19,29 @@ app.listen(port, () => {
     console.log(`http://localhost:${port}`);
 });
 
+
 dbConnect();
+
+const io = socket(port, {
+    cors: {
+        origin: "*",
+        credentials: true,
+    },
+});
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+    global.chatSocket = socket;
+    socket.on("add-user", (userId) => {
+        onlineUsers.set(userId, socket.id);
+        console.log(`User ${userId} connected`);
+    })
+socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+        socket.to(sendUserSocket).emit("receive-msg", data.msg);
+    }
+});
+
+});
