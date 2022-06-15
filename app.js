ce910d1c83c9016368919f22437bb6c0eece13fe
Bryam11 <xavierchuchuca18@gmail.com>
2022-06-15 18:16:44 -0500

    Change declaration server listen

--- app.js
+++ app.js
@@ -13,19 +13,23 @@ app.use(express.json())
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
-});
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
+
+// app.listen(port, () => {
+//     console.log(`http://localhost:${port}`);
+// });
 
 
 dbConnect();
 
-const io = socket(5000, {
+const io = socket(server, {
     cors: {
         origin: "*",
-        credentials: true,
+        credentials: false,
     },
 });
 
